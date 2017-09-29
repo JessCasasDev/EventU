e87a90c4d43b9d39a2e36c12a72249d5c7dcf588
AceMaster <ace7ace@hotmail.com>
2017-09-28 20:26:38 -0500

    Map bug when refresh near events fixed, basic logic in lign added, css variables updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, Events } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginPage } from '../pages/login/login';
-import { SignupPage } from '../pages/signup/signup';
 import { NearEventsPage } from "../pages/near-events/near-events"
 import { CreateEventsPage } from "../pages/create-events/create-events"
 import { MyEventsPage } from '../pages/my-events/my-events';
@@ -19,19 +18,23 @@ export class MyApp {
   rootPage: any = LoginPage;
 
   pages: Array<{title: string, icon:string, component: any}>;
+  user: {name:string, avatar: String};
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, 
+              public splashScreen: SplashScreen, public events: Events) {
     this.initializeApp();
-
     this.pages = [
       {title: "Eventos Cercanos", icon:"home",component: NearEventsPage},
       {title: "Crear Evento", icon:"add",component: CreateEventsPage},
-      { title: "Mis Eventos", icon: "person", component: MyEventsPage },
+      {title: "Mis Eventos", icon: "person", component: MyEventsPage },
     ];
-
+    events.subscribe('user:login', (name,avatar) => {
+      this.setUser(name,avatar);
+    });
   }
 
   initializeApp() {
+    this.user = {name:"",avatar:"assets/img/profiletest.png"}
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -41,6 +44,12 @@ export class MyApp {
   }
 
   openPage(page) {
-    this.nav.setRoot(page.component);
+    if(!(this.nav.getActive().name == "NearEventsPage" && this.nav.getActive().name == page.component.name))
+      this.nav.setRoot(page.component);
+  }
+
+  public setUser(name,avatar){
+    this.user.name = name;
+    //TODO Uncomment if(avatar != "") this.user.avatar = avatar;
   }
 }
